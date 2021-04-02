5b3f9522bfd530b022379a38c38492db9565327c
rekha4371 <rekha4371@gmail.com>
2021-04-02 14:37:07 +0530

    Added Pig

--- sketch.js
+++ sketch.js
@@ -21,6 +21,7 @@ function setup(){
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
+    pig1 = new Pig(250, 350);
     log1 = new Log(810,260,300, PI/2);
 
     box3 = new Box(700,240,70,70);
@@ -48,7 +49,7 @@ function draw(){
     ground.display();
     pig1.display();
     log1.display();
-
+    pig2.display();
     box3.display();
     box4.display();
     pig3.display();
